402f6a479af88bd4c359486fe762e59c166939ee
ReleMoralo <st10399732@rcconnect.edu.za>
2026-05-04 00:11:48 +0200

    Initial commit,This includes some php code ,html. Its not yet working but it will be

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Login</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <nav class="navbar">
+
+        <div class="logo">
+            <a href="index.html">Second Hand Fit</a>
+        </div>
+
+    </nav>
+
+    <section class="join">
+
+        <h2>Login</h2>
+
+        <form>
+
+            <label>Email</label>
+            <input type="email">
+
+            <label>Password</label>
+            <input type="password">
+
+            <button class="shop-btn">
+                Login
+            </button>
+
+        </form>
+
+    </section>
+
+</body>
+
+</html>
